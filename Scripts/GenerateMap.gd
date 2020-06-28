49d346ac5f9f2c2261fa0638adfcc9b2a7139759
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-06-28 09:47:05 -0500

    working on some things

--- Scripts/GenerateMap.gd
+++ Scripts/GenerateMap.gd
@@ -3,16 +3,16 @@ extends TileMap
 var grid = []
 
 func _ready():
-	grid.resize(17)
-	for n in 17:
+	grid.resize(24)
+	for n in 24:
 		grid[n] = []
-		grid [n].resize(17)
-		for m in 17:
+		grid [n].resize(24)
+		for m in 24:
 			if (n%15 == 0 or m%8 == 0) and randi()%20 != 0:
 				grid[n][m] = 0
 			else:
 				grid[n][m] = -1
 	
-	for n in range (0, 16):
-		for m in range (0, 16): 
+	for n in range (0, 23):
+		for m in range (0, 23): 
 			set_cell(n, m, grid[n][m])
