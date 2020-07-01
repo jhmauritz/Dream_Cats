ccc688ea08447c80823a7ac255037850eaac26c3
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-06-30 21:58:02 -0500

    its not a state machine but whatever

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,41 +1,39 @@
 extends KinematicBody2D
-
+ 
 const UP = Vector2(0, -1)
 const SLOPE_STOP = 64
 
-export (int) var move_speed = 5*96
-
 var velocity = Vector2()
+var move_speed = 96*5
 var gravity = 1200
-var jump_velocity = -720
+var jump_velocit = -720
 var is_grounded
 
 onready var raycasts = $RayCasts
-
 onready var anim_player = $CharacterRig/AnimationPlayer
 
 func _physics_process(delta):
 	_get_input()
 	velocity.y += gravity * delta
-	move_and_slide(velocity, UP, SLOPE_STOP)
+	
+	velocity = move_and_slide(velocity, UP, SLOPE_STOP)
 	
 	is_grounded = _check_is_grounded()
 	_assign_animation()
 
 func _input(event):
-	if event.is_action_pressed("jump"):
-		velocity.y = jump_velocity
+	if event.is_action_pressed("jump") && is_grounded:
+		velocity.y = jump_velocit
 
 func _get_input():
-	var move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
-	velocity.x = lerp(velocity.x, move_speed * move_direction, _get_h_weight())
-	if move_direction != 0:
-		$CharacterRig.scale.x = move_direction
+	var move_dir = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
+	velocity.x = lerp(velocity.x, move_speed * move_dir, _get_h_weight())
+	if move_dir != 0:
+		$CharacterRig.scale.x = move_dir
 
 func _get_h_weight():
 	return 0.2 if is_grounded else 0.1
 
-#fix next
 func _check_is_grounded():
 	for raycast in raycasts.get_children():
 		if raycast.is_colliding():
@@ -45,9 +43,9 @@ func _check_is_grounded():
 func _assign_animation():
 	var anim = "IdleStanceAnim"
 	
-	if is_grounded:
+	if !is_grounded:
 		anim = "Jump"
-	elif velocity.x != 0: 
+	elif velocity.x != 0:
 		anim = "WalkAnim"
 	
 	if anim_player.assigned_animation != anim:
