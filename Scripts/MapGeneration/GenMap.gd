d0cc9af5b40b68bd4dbb38cce6b162d9b28c556f
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-07-06 22:29:25 -0500

    changes to map generation: starting

--- Scripts/MapGeneration/GenMap.gd
+++ Scripts/MapGeneration/GenMap.gd
@@ -1,3 +1,19 @@
 extends Node
+class_name Generator
 
+const STEP := [Vector2.LEFT, Vector2.LEFT, Vector2.RIGHT, Vector2.RIGHT, Vector2.DOWN]
 
+export (PackedScene) var Rooms := preload("res://Scenes/Generation_Rooms/PackedRoomScene.tscn")
+export var grid_size := Vector2(8,6)
+
+var _rng := RandomNumberGenerator.new()
+var _rooms: Rooms = null
+var _player: KinematicBody2D = null
+var _state := {}
+var _horiz_chance := 0.0
+var _cam_limits := {}
+var _resolution := OS.window_size
+var _running := false
+
+onready var camera: Camera2D = $Camera2D
+onready var tween: Tween = $Camera2D/Tween
